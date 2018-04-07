d451faaa6f7d394f033d059626070daccd2d8c6c
Rafał <rafalrojek96@gmail.com>
2018-04-08 00:27:40 +0200

    Added sql query which add required table

---
+++ TransactionTable.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Transaction]
+(
+	[IdTransaction] INT NOT NULL PRIMARY KEY identity,
+	[Recesiver] NVARCHAR(MAX) NOT NULL,
+	[Sender] NVARCHAR(MAX) NOT NULL,
+	[Amount] DECIMAL NOT NULL,
+	[Date] DATETIME NOT NULL,
+	[Type] NVARCHAR(MAX) NOT NULL
+)
